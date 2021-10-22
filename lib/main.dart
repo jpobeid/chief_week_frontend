dbea167f24e48b4bd9054066f39b2e9619c6994e
JP Obeid <jpobeid91@gmail.com>
2021-10-22 00:49:05 -0700

    Adding build dir for web

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
   runApp(const ProviderScope(
     child: MaterialApp(
+      title: 'Chief weekly email',
       home: HomePage(),
     ),
   ));
